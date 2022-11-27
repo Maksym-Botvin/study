0b226033aca76b9d01b1193f1b4063f0dffbb1a2
Maksym_Botvin <botvinmaxim@gmail.com>
2022-11-28 00:58:51 +0200

    LJ3-189

--- src/main/java/com/botvin/model/Engine.java
+++ src/main/java/com/botvin/model/Engine.java
@@ -1,38 +1,24 @@
 package com.botvin.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Random;
 
+@Setter
+@Getter
 public class Engine {
-    Random random = new Random();
-
+    private Random random = new Random();
     private int power;
     private String type;
-
-    public void setPower(int power) {
-        this.power = power;
-    }
-
-    public int getPower() {
-        return power;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getType() {
-        return type;
-    }
 
     public Engine(String type) {
         this.power = random.nextInt(0, 1000);
         this.type = type;
-
     }
 
     @Override
     public String toString() {
         return "power = " + power + ", type = " + type;
     }
-
 }
