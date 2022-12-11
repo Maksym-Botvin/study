04a8a9f6c04497933ca93377471f2a07b970ae2d
Maksym_Botvin <botvinmaxim@gmail.com>
2022-12-12 00:00:19 +0200

    LJ3-245

---
+++ src/main/java/com/botvin/model/UserInputException.java
@@ -0,0 +1,10 @@
+package com.botvin.model;
+
+public class UserInputException extends Exception {
+    public UserInputException() {
+
+    }
+    public UserInputException(final String message) {
+        super(message);
+    }
+}
