41b761fb1928d349e11a9fc2c80f377c1669c86b
Maksym_Botvin <botvinmaxim@gmail.com>
2022-12-25 11:16:28 +0200

    added access modifiers

--- src/main/java/com/botvin/container/Node.java
+++ src/main/java/com/botvin/container/Node.java
@@ -3,9 +3,9 @@ package com.botvin.container;
 import com.botvin.model.Car;
 
 public class Node<T extends Car> {
-    T car;
-    Node pPrev;
-    Node pNext;
+    protected T car;
+    protected Node pPrev;
+    protected Node pNext;
 
     public Node(T car) {
         this.car = car;
